94ccc0c9cf1689ccc14246bc90b4b83ded670361
VuTuanCanh <tuancanhnb@gmail.com>
2023-05-10 02:54:35 +0700

    add library axios + materialUI; in progressing axios config

---
+++ src/types/userType.ts
@@ -0,0 +1,22 @@
+export interface IUser {
+  address: {
+    geolocation: {
+      lat: number;
+      long: number;
+    };
+    city: string;
+    street: string;
+    number: number;
+    zipcode: string;
+  };
+  id: number;
+  email: string;
+  username: string;
+  password: string;
+  name: {
+    firstname: string;
+    lastname: string;
+  };
+  phone: string;
+  __v: number;
+}
