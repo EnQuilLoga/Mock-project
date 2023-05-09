c13264d3a0034109e4f425725fd57d2d6b28855c
VuTuanCanh <tuancanhnb@gmail.com>
2023-05-09 22:42:11 +0700

    update

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import junnoLogo from "../../assets/junno-logo.jpg";
 import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
+import bannerSubmenu from "../../assets/custom_banner_submenu.jpg";
 
 export interface IHeaderProps {}
 
@@ -65,9 +66,9 @@ export default function Header(props: IHeaderProps) {
                 </div>
               </div>
             </div>
-            <nav className="my-5">
+            <nav>
               <ul className="flex items-center text-xs gap-x-3 lg:gap-x-5 lg:text-sm xl:gap-x-7 xl:text-base">
-                <li className="hover:text-red-500 font-bold">
+                <li className="hover:text-red-500 font-bold my-5">
                   <NavLink
                     to={"/"}
                     className={({ isActive, isPending }) =>
@@ -77,9 +78,9 @@ export default function Header(props: IHeaderProps) {
                     HOME
                   </NavLink>
                 </li>
-                {/* Bags & Shoes */}
+                {/* nav1 */}
                 <li className="group relative">
-                  <div className="group-hover:text-red-500 font-bold">
+                  <div className="group-hover:text-red-500 font-bold py-5">
                     <NavLink
                       to={"bags-shoes"}
                       className={({ isActive, isPending }) =>
@@ -89,10 +90,10 @@ export default function Header(props: IHeaderProps) {
                       BAGS & SHOES <i className="fas fa-angle-down text-xs"></i>
                     </NavLink>
                   </div>
-                  <div className="bg-slate-400 text-white absolute opacity-0 -z-10 translate-y-0 invisible transition ease-linear delay-300 group-hover:opacity-100 group-hover:visible group-hover:z-50">
-                    <div className="flex">
+                  <div className="bg-white text-gray-500 border absolute opacity-0 -z-10 translate-y-0 invisible transition ease-linear delay-300 group-hover:opacity-100 group-hover:visible group-hover:z-50 ">
+                    <div>
                       <div>
-                        <Link to={"shoes"} className="hover:text-red-500">
+                        <Link to={"shoes"} className="hover:text-red-500 font-bold text-black">
                           SHOES
                         </Link>
                         <ul>
@@ -114,7 +115,10 @@ export default function Header(props: IHeaderProps) {
                         </ul>
                       </div>
                       <div>
-                        <Link to={"luggage-bags"} className="hover:text-red-500">
+                        <Link
+                          to={"luggage-bags"}
+                          className="hover:text-red-500 font-bold text-black"
+                        >
                           LUGGAGE & BAGS
                         </Link>
                         <ul>
@@ -133,7 +137,10 @@ export default function Header(props: IHeaderProps) {
                         </ul>
                       </div>
                       <div>
-                        <Link to={"other-bags-accessories"} className="hover:text-red-500">
+                        <Link
+                          to={"other-bags-accessories"}
+                          className="hover:text-red-500 font-bold text-black"
+                        >
                           OTHER BAGS & ACCESSORIES
                         </Link>
                         <ul>
@@ -155,72 +162,80 @@ export default function Header(props: IHeaderProps) {
                         </ul>
                       </div>
                     </div>
+                    <div>
+                      <img src={bannerSubmenu} alt="banner submenu" />
+                    </div>
                   </div>
                 </li>
-                {/* End Bags & Shoes */}
+                {/* end nav1 */}
                 <li className="group relative">
-                  <div className="group-hover:text-red-500 font-bold">
+                  <div className="group-hover:text-red-500 font-bold py-5">
                     <NavLink
-                      to={"shoes"}
+                      to={"bags-shoes"}
                       className={({ isActive, isPending }) =>
-                        isPending ? "pending" : isActive ? "text-red-500" : ""
+                        isPending ? "pending " : isActive ? "text-red-500" : ""
                       }
                     >
                       SHOES <i className="fas fa-angle-down text-xs"></i>
                     </NavLink>
                   </div>
-                  <div className="bg-slate-400 text-white absolute top-auto opacity-0 -z-10 invisible transition ease-linear delay-300 group-hover:opacity-100 group-hover:visible group-hover:z-50">
-                    <ul>
-                      <li className="hover:text-red-500">
-                        <Link to={"women-shoes"}>Women Shoes</Link>
-                      </li>
-                      <li className="hover:text-red-500">
-                        <Link to={"men-shoes"}>Men Shoes</Link>
-                      </li>
-                      <li className="hover:text-red-500">
-                        <Link to={"boots"}>Boots</Link>
-                      </li>
-                      <li className="hover:text-red-500">
-                        <Link to={"casual-shoes"}>Casual Shoes</Link>
-                      </li>
-                      <li className="hover:text-red-500">
-                        <Link to={"flip-flops"}>Flip Flops</Link>
-                      </li>
-                    </ul>
+                  <div className="bg-white text-gray-500 border absolute opacity-0 -z-10 translate-y-0 invisible transition ease-linear delay-300 group-hover:opacity-100 group-hover:visible group-hover:z-50">
+                    <div>
+                      <ul>
+                        <li className="hover:text-red-500">
+                          <Link to={"women-shoes"}>Women Shoes</Link>
+                        </li>
+                        <li className="hover:text-red-500">
+                          <Link to={"men-shoes"}>Men Shoes</Link>
+                        </li>
+                        <li className="hover:text-red-500">
+                          <Link to={"boots"}>Boots</Link>
+                        </li>
+                        <li className="hover:text-red-500">
+                          <Link to={"casual-shoes"}>Casual Shoes</Link>
+                        </li>
+                        <li className="hover:text-red-500">
+                          <Link to={"flip-flops"}>Flip Flops</Link>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
+                  {/* end submenu */}
                 </li>
                 <li className="group relative">
-                  <div className="group-hover:text-red-500 font-bold">
+                  <div className="group-hover:text-red-500 font-bold py-5">
                     <NavLink
-                      to={"other-bags-accessories"}
+                      to={"bags-shoes"}
                       className={({ isActive, isPending }) =>
-                        isPending ? "pending" : isActive ? "text-red-500" : ""
+                        isPending ? "pending " : isActive ? "text-red-500" : ""
                       }
                     >
                       OTHER BAGS & ACCESSORIES <i className="fas fa-angle-down text-xs"></i>
                     </NavLink>
                   </div>
-                  <div className="bg-slate-400 text-white absolute top-auto opacity-0 -z-10 invisible transition ease-linear delay-300 group-hover:opacity-100 group-hover:visible group-hover:z-50">
-                    <ul>
-                      <li className="hover:text-red-500">
-                        <Link to={"cosmetic-bags-cases"}>Cosmetic Bags & Cases</Link>
-                      </li>
-                      <li className="hover:text-red-500">
-                        <Link to={"wallets-card-holders"}>Wallets & Card Holders</Link>
-                      </li>
-                      <li className="hover:text-red-500">
-                        <Link to={"luggage-covers"}>Luggage Covers</Link>
-                      </li>
-                      <li className="hover:text-red-500">
-                        <Link to={"passport-covers"}>Passport Covers</Link>
-                      </li>
-                      <li className="hover:text-red-500">
-                        <Link to={"bag-parts-accessories"}>Bag Parts & Accessories</Link>
-                      </li>
-                    </ul>
+                  <div className="bg-white text-gray-500 border absolute opacity-0 -z-10 translate-y-0 invisible transition ease-linear delay-300 group-hover:opacity-100 group-hover:visible group-hover:z-50">
+                    <div>
+                      <ul>
+                        <li className="hover:text-red-500">
+                          <Link to={"cosmetic-bags-cases"}>Cosmetic Bags & Cases</Link>
+                        </li>
+                        <li className="hover:text-red-500">
+                          <Link to={"wallets-card-holders"}>Wallets & Card Holders</Link>
+                        </li>
+                        <li className="hover:text-red-500">
+                          <Link to={"luggage-covers"}>Luggage Covers</Link>
+                        </li>
+                        <li className="hover:text-red-500">
+                          <Link to={"passport-covers"}>Passport Covers</Link>
+                        </li>
+                        <li className="hover:text-red-500">
+                          <Link to={"bag-parts-accessories"}>Bag Parts & Accessories</Link>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </li>
-                <li className="hover:text-red-500 font-bold">
+                <li className="hover:text-red-500 font-bold py-5">
                   <NavLink
                     to={"contact-us"}
                     className={({ isActive, isPending }) =>
@@ -230,7 +245,7 @@ export default function Header(props: IHeaderProps) {
                     CONTACT US
                   </NavLink>
                 </li>
-                <li className="hover:text-red-500 font-bold">
+                <li className="hover:text-red-500 font-bold py-5">
                   <NavLink
                     to={"about-us"}
                     className={({ isActive, isPending }) =>
