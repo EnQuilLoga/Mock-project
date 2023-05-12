d645ed47233cfb25c11463a364340cffdf4e9f45
DuzBlack <adung9990@gmail.com>
2023-05-12 17:59:26 +0700

    update component product

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
+import ProductDemo from "./components/product-demo/productDemo";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
+      <ProductDemo></ProductDemo>
       <Footer />
+      
     </div>
   );
 }
--- src/components/product-demo/productDemo.tsx
+++ src/components/product-demo/productDemo.tsx
@@ -1,6 +1,8 @@
-
 import * as React from "react";
 import styles from '../product-demo/productDemo.module.css';
+import anhgiay from "../../assets/custom-nike-air-force-1-low-by-you-removebg-preview.png"
+import { Button } from '@mui/material';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export interface listProduct {}
 
@@ -12,18 +14,100 @@ export default function ProductDemo(props: listProduct){
                 <p>Add our products to weekly line up</p>
             </div>
             <div className={styles.kindProduct}>
-                <button className={styles.button}>Women Shoes</button>
+                <Button variant="outlined">Women Shoes</Button>
+                <Button variant="outlined">Men Shoes</Button>
+                <Button variant="outlined">Casual Shoes</Button>
+                {/* <button className={styles.button}>Women Shoes</button>
                 <button className={styles.button}>Men Shoes</button>
-                <button className={styles.button}>Causual Shoes</button>
+                <button className={styles.button}>Causual Shoes</button> */}
             </div>
             <div className={styles.listProduct}>
                 <div className={styles.product}>
-                <img src="" alt="" />
+                    <img className="" src={anhgiay} alt="giay1" />
+                    <p className="productDetail">Water And Wind Resistant Insulated Jacket</p>
+                    <p className="rate">
+                    {/* <FontAwesomeIcon icon="far fa-star" /> */}
+                        <i className="fa-regular fa-star text-yellow"></i>
+                        <i className="fa-regular fa-star text-yellow"></i>
+                        <i className="fa-regular fa-star text-yellow"></i>
+                        <i className="fa-regular fa-star"></i>
+                    </p>
+                    <p className="price">100$</p>
+                    <p className="addToCart">i</p>
+                </div>
+
+                <div className={styles.product}>
+                    <img src={anhgiay} alt="" />
+                    <p className="productDetail">Nike</p>
+                    <p className="rate">5star</p>
+                    <p className="price">100$</p>
+                    <p className="addToCart">i</p>
+                </div>
+
+                <div className={styles.product}>
+                <img src={anhgiay} alt="" />
                 <p className="productDetail">Nike</p>
                 <p className="rate">5star</p>
                 <p className="price">100$</p>
                 <p className="addToCart">i</p>
                 </div>
+                
+                <div className={styles.product}>
+                <img src={anhgiay} alt="" />
+                <p className="productDetail">Nike</p>
+                <p className="rate">5star</p>
+                <p className="price">100$</p>
+                <p className="addToCart">i</p>
+                </div>
+
+                <div className={styles.product}>
+                <img src={anhgiay} alt="" />
+                <p className="productDetail">Nike</p>
+                <p className="rate">5star</p>
+                <p className="price">100$</p>
+                <p className="addToCart">i</p>
+                </div>
+
+                <div className={styles.product}>
+                <img src={anhgiay} alt="" />
+                <p className="productDetail">Nike</p>
+                <p className="rate">5star</p>
+                <p className="price">100$</p>
+                <p className="addToCart">i</p>
+                </div>
+
+                <div className={styles.product}>
+                <img src={anhgiay} alt="" />
+                <p className="productDetail">Nike</p>
+                <p className="rate">5star</p>
+                <p className="price">100$</p>
+                <p className="addToCart">i</p>
+                </div>
+
+                <div className={styles.product}>
+                <img src={anhgiay} alt="" />
+                <p className="productDetail">Nike</p>
+                <p className="rate">5star</p>
+                <p className="price">100$</p>
+                <p className="addToCart">i</p>
+                </div>
+
+                <div className={styles.product}>
+                <img src={anhgiay} alt="" />
+                <p className="productDetail">Nike</p>
+                <p className="rate">5star</p>
+                <p className="price">100$</p>
+                <p className="addToCart">i</p>
+                </div>
+
+                <div className={styles.product}>
+                <img src={anhgiay} alt="" />
+                <p className="productDetail">Nike</p>
+                <p className="rate">5star</p>
+                <p className="price">100$</p>
+                <p className="addToCart">i</p>
+                </div>
+
             </div>
         </div>
     )
