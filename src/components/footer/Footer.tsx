3a6954f87b48060a303f92ff43b6530d249533f1
VuTuanCanh <tuancanhnb@gmail.com>
2023-05-12 20:55:42 +0700

    fix footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,8 +4,9 @@ export interface IFooterProps {}
 
 export default function Footer(props: IFooterProps) {
   return (
-    <footer>
-      <h1 className="bg-green-500">Footer</h1>
+    <footer className="flex">
+      <div className="bg-gray-100 w-1/3">hahhahahaha</div>
+      <div className="bg-gray-300 w-2/3">hihihhihihi</div>
     </footer>
   );
 }
