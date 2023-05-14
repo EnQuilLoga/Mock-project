374029b6d439401715b9a5ce9a4d96daa5a78178
HaiBe1997 <haiub1997@gmail.com>
2023-05-14 23:12:39 +0700

    style ContactUs

--- src/components/contact/ContactUs.tsx
+++ src/components/contact/ContactUs.tsx
@@ -4,24 +4,24 @@ export interface IContactUsProps {
 }
 export default function ContactUs (props: IContactUsProps) {
 return (
-    <div className="container">
+    <div className="container m-auto">
         <div className=" flex flex-wrap"> 
             {/* Store */}
-            <div className="store float-left w-1/4">
+            <div className="store sm:w-full lg:w-1/4 ">
                 <div className="px-4" >
                     <h4 className="text-2xl font-semibold mb-8">Store Information</h4>
                     <div className="font-light">
                         {/* Map */}
-                        <div className="block">
-                            <div className="icon float-left w-14">
+                        <div className="flex">
+                            <div className="icon  w-14">
                                 <i className="fas fa-map-marker-alt text-2xl text-slate-400 "></i>
                             </div>
                             <div className="data truncate w-auto">Junno Responsive Prestashop Theme <br/> United States</div>
                         </div>
                         <hr className="my-4"/>
                         {/* Call */}
-                        <div className="block">
-                            <div className="icon float-left w-14">
+                        <div className="flex">
+                            <div className="icon  w-14">
                                 <i className="fas fa-phone-alt text-2xl text-slate-400"></i>
                             </div>
                             <div className="data truncate w-auto">
@@ -32,8 +32,8 @@ return (
                         </div>
                         <hr className="my-4"/>
                         {/* Mail */}
-                        <div className="block">
-                            <div className="icon float-left w-14">
+                        <div className="flex">
+                            <div className="icon  w-14">
                                 <i className="fas fa-envelope text-2xl text-slate-400"></i>
                             </div>
                             <div className="data truncate w-auto">
@@ -47,17 +47,17 @@ return (
             </div>
 
             {/* Contact */}
-            <div className="contact float-left w-3/4">
+            <div className="contact sm:w-full sm:mt-16  lg:w-3/4">
                 <div className="px-4">
                     <div>
                         <h3 className="text-2xl font-semibold mb-8">Contact Us</h3>
                     </div>
                     <div  className="font-light">
                          {/* Subject */}
-                        <div className="contact-group mb-4 ">
+                        <div className="flex mb-4 ">
                             <label className="text-left w-1/4">Subject</label>
-                            <div className="contact-controll float-left w-1/2 border">
-                                <select name="" id="" className="w-full">
+                            <div className=" w-1/2 border">
+                                <select name="" id="" className="w-full py-2 px-4">
                                 <option value="" className="" >Customer service</option>
                                 <option value="" className="">Webmaster</option> 
                                 </select>
@@ -65,32 +65,32 @@ return (
                         </div>
 
                         {/* Email */}
-                        <div className="contact-group mb-4">
-                            <label className="text-left">Email address</label>
-                            <div className="contact-controll float-left w-1/2 border">
-                                <input type="email" placeholder="your@email.com" className="w-full" />
+                        <div className="flex mb-4">
+                            <label className="text-left w-1/4">Email address</label>
+                            <div className=" w-1/2 border">
+                                <input type="email" placeholder="your@email.com" className="w-full py-2 px-4" />
                             </div>
                         </div>
 
                         {/* Attachment */}
-                        <div className="contact-group mb-4">
-                            <label className="text-left">Attachment</label>
-                            <div className="contact-controll float-left w-1/2 border">
-                                <input type="file" name="file" className="w-full"/>
+                        <div className="flex mb-4">
+                            <label className="text-left w-1/4">Attachment</label>
+                            <div className=" w-1/2 border">
+                                <input type="file" name="file" className="w-full "/>
                             </div>
                         </div>  
 
                         {/* Message */}
-                        <div className="contact-group mb-4"> 
-                            <label className="text-left">Message</label>
-                            <div className="float-left w-1/2 border">
+                        <div className="flex mb-4"> 
+                            <label className="text-left w-1/4">Message</label>
+                            <div className=" w-1/2 border">
                                 <textarea name="" id="" placeholder="How can hlep you???" className="w-full"></textarea>
                             </div>
                         </div>
 
                         {/* Send */}
-                        <div className="">
-                            <input type="submit" value="Send" />
+                        <div className="float-right">
+                            <input type="submit" name="" value="Send" className="py-1 px-5 bg-black text-white rounded" />
                         </div>
                     </div>
                 </div>
