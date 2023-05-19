a4d8012d1b55a9bad96b25cd7a3e593b94581480
HaiBe1997 <haiub1997@gmail.com>
2023-05-19 20:33:55 +0700

    part2

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import Product from "../productDetail/ProductDetail";
+import ProductDemo from "../product-demo/productDemo";
 import ContactUs from "../contact/ContactUs";
 import AboutUs from "../contact/AboutUs";
 import LatestBlogs from "../Latest/LatestBlogs";
