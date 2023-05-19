2a5dd55063459a8573a9c4381dfb2c6434a45ef1
DuzBlack <adung9990@gmail.com>
2023-05-19 19:33:06 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
-import ProductDemo from "./components/product-demo/productDemo";
 
 function App() {
   return (
